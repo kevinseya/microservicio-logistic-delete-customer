6a08d4f86c869eb5da79a3163945e19bdc7d2141
kevinseya <kevinseya@gmail.com>
2025-02-07 03:31:17 -0500

    perf: :white_check_mark: Update management of websocket

---
+++ src/main/java/com/microserviciologistic/deletecustomer/websocket/WebSocketMessageHandler.java
@@ -0,0 +1,19 @@
+package com.microserviciologistic.deletecustomer.websocket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microserviciologistic.deletecustomer.model.WebSocketEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebSocketMessageHandler {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public void processMessage(String message) {
+        try {
+            WebSocketEvent event = objectMapper.readValue(message, WebSocketEvent.class);
+            System.out.println("Event recived: " + event);
+        } catch (Exception e) {
+            System.err.println(" Error to process the message: " + e.getMessage());
+        }
+    }
+}
